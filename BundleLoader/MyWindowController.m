e14784d9044df0b610804c623edf106c47a7c46b
Steve Moser <contact@stevemoser.org>
2015-11-26 20:55:29 -0500

    Add OSX SDK 10.7 (411.5) Docset changes

--- BundleLoader/MyWindowController.m
+++ BundleLoader/MyWindowController.m
@@ -1,52 +1,49 @@
 /*
-
-File: MyWindowController.m
-
-Abstract: Source file for this sample's main NSWindowController.
-
-Version: 1.0
-
-Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
-Inc. ("Apple") in consideration of your agreement to the
-following terms, and your use, installation, modification or
-redistribution of this Apple software constitutes acceptance of these
-terms.  If you do not agree with these terms, please do not use,
-install, modify or redistribute this Apple software.
-
-In consideration of your agreement to abide by the following terms, and
-subject to these terms, Apple grants you a personal, non-exclusive
-license, under Apple's copyrights in this original Apple software (the
-"Apple Software"), to use, reproduce, modify and redistribute the Apple
-Software, with or without modifications, in source and/or binary forms;
-provided that if you redistribute the Apple Software in its entirety and
-without modifications, you must retain this notice and the following
-text and disclaimers in all such redistributions of the Apple Software. 
-Neither the name, trademarks, service marks or logos of Apple Inc.
-may be used to endorse or promote products derived from the Apple
-Software without specific prior written permission from Apple.  Except
-as expressly stated in this notice, no other rights or licenses, express
-or implied, are granted by Apple herein, including but not limited to
-any patent rights that may be infringed by your derivative works or by
-other works in which the Apple Software may be incorporated.
-
-The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
-MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
-OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-
-IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
-OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
-MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
-AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-Copyright © 2007 Apple Inc. All Rights Reserved
-
-*/
+     File: MyWindowController.m 
+ Abstract: Sample's main NSWindowController. 
+  Version: 1.1 
+  
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple 
+ Inc. ("Apple") in consideration of your agreement to the following 
+ terms, and your use, installation, modification or redistribution of 
+ this Apple software constitutes acceptance of these terms.  If you do 
+ not agree with these terms, please do not use, install, modify or 
+ redistribute this Apple software. 
+  
+ In consideration of your agreement to abide by the following terms, and 
+ subject to these terms, Apple grants you a personal, non-exclusive 
+ license, under Apple's copyrights in this original Apple software (the 
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple 
+ Software, with or without modifications, in source and/or binary forms; 
+ provided that if you redistribute the Apple Software in its entirety and 
+ without modifications, you must retain this notice and the following 
+ text and disclaimers in all such redistributions of the Apple Software. 
+ Neither the name, trademarks, service marks or logos of Apple Inc. may 
+ be used to endorse or promote products derived from the Apple Software 
+ without specific prior written permission from Apple.  Except as 
+ expressly stated in this notice, no other rights or licenses, express or 
+ implied, are granted by Apple herein, including but not limited to any 
+ patent rights that may be infringed by your derivative works or by other 
+ works in which the Apple Software may be incorporated. 
+  
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE 
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION 
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS 
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND 
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS. 
+  
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL 
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, 
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED 
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE), 
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE. 
+  
+ Copyright (C) 2011 Apple Inc. All Rights Reserved. 
+  
+ */
 
 #import "MyWindowController.h"
 #import "MyBundle.h"
@@ -54,10 +51,10 @@ Copyright © 2007 Apple Inc. All Rights Reserved
 @implementation MyWindowController
 
 // all of our target built bundles for this sample will start with:
-NSString* const kPrefixBundleIDStr = @"com.apple.AppKit.bundleexample";
+NSString* const kPrefixBundleIDStr = @"com.yourcompany.bundleexample";
 NSString* const kSearchStr = @"Searching for bundles...";
 	
-	
+
 // -------------------------------------------------------------------------------
 //	handleActivate:
 // -------------------------------------------------------------------------------
@@ -72,7 +69,7 @@ NSString* const kSearchStr = @"Searching for bundles...";
 		// one of our bundle windows is closing, update the our 'hasOpenWindows' flag
 		NSEnumerator* bundleListEnum = [[arrayController arrangedObjects] objectEnumerator];
 		NSDictionary* bundleDict;
-		while (bundleDict = [bundleListEnum nextObject])
+		while ((bundleDict = [bundleListEnum nextObject]))
 		{
 			MyBundle* curBundle = [bundleDict objectForKey:@"bundleInstance"];
 			if ([curBundle isOpen])
@@ -103,7 +100,7 @@ NSString* const kSearchStr = @"Searching for bundles...";
 		// we are closing our main application window, so close all the bundle windows		
 		NSDictionary* bundleDict;
 		NSEnumerator* bundleListEnum = [[arrayController arrangedObjects] objectEnumerator];
-		while (bundleDict = [bundleListEnum nextObject])
+		while ((bundleDict = [bundleListEnum nextObject]))
 		{
 			MyBundle* curBundle = [bundleDict objectForKey:@"bundleInstance"];
 			[curBundle close];
@@ -149,7 +146,7 @@ NSString* const kSearchStr = @"Searching for bundles...";
 	
 	MyBundle* curBundle = nil;
 	NSEnumerator* instanceEnum = [bundleInstanceList objectEnumerator];
-	while (curBundle = [instanceEnum nextObject])
+	while ((curBundle = [instanceEnum nextObject]))
 	{
 		// add all the discovered bundles to our array controller/table view
 		NSMutableDictionary* bundleEntryDict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
@@ -191,14 +188,14 @@ NSString* const kSearchStr = @"Searching for bundles...";
 	
     NSString* currPath;
 	NSEnumerator* searchPathEnum = [bundleSearchPaths objectEnumerator];
-	while (currPath = [searchPathEnum nextObject])
+	while ((currPath = [searchPathEnum nextObject]))
     {
         NSDirectoryEnumerator *bundleEnum;
         NSString *currBundlePath;
         bundleEnum = [[NSFileManager defaultManager] enumeratorAtPath:currPath];
         if (bundleEnum)
         {
-            while (currBundlePath = [bundleEnum nextObject])
+            while ((currBundlePath = [bundleEnum nextObject]))
             {
                 if ([[currBundlePath pathExtension] isEqualToString:@"bundle"])
                 {
@@ -213,7 +210,7 @@ NSString* const kSearchStr = @"Searching for bundles...";
 	NSRange searchRange = NSMakeRange(0, [kPrefixBundleIDStr length]);
 	
 	NSEnumerator* pathEnum = [bundlePaths objectEnumerator];
-    while (currPath = [pathEnum nextObject])
+    while ((currPath = [pathEnum nextObject]))
     {
         NSBundle* currBundle = [NSBundle bundleWithPath:currPath];
         if (currBundle)
@@ -363,7 +360,7 @@ NSString* const kSearchStr = @"Searching for bundles...";
 {
 	NSEnumerator* bundleListEnum = [[arrayController arrangedObjects] objectEnumerator];
 	NSDictionary* bundleDict;
-	while (bundleDict = [bundleListEnum nextObject])
+	while ((bundleDict = [bundleListEnum nextObject]))
 	{
 		MyBundle* curBundle = [bundleDict objectForKey:@"bundleInstance"];
 		if ([curBundle isOpen])
